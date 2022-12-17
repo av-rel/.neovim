3a4aec942456d405b3c0395fe5e734aaf8a67b34
Avrel <106798140+Avrel3@users.noreply.github.com>
2022-12-17 12:24:14 +0545

    Setup no lsp

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -17,47 +17,22 @@ return require('packer').startup(function(use)
 
   use 'lewis6991/impatient.nvim'
   use 'kyazdani42/nvim-web-devicons'
-  use 'nvim-lua/popup.nvim'
 
-  use 'Mofiqul/vscode.nvim'
-  use 'nvim-lualine/lualine.nvim'
+  use 'folke/tokyonight.nvim'
+  use { 'Avimitin/nerd-galaxyline', requires = { 'glepnir/galaxyline.nvim' } }
+
   use 'nvim-tree/nvim-tree.lua'
+  use 'ThePrimeagen/harpoon'
   use {
     'nvim-telescope/telescope.nvim',
-    requires = { 'nvim-lua/plenary.nvim' }
+    requires = { 'nvim-lua/plenary.nvim', 'nvim-lua/popup.nvim' }
   }
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate'
-  }
+  use 'nvim-treesitter/nvim-treesitter'
 
+  use 'github/copilot.vim'
   use 'windwp/nvim-autopairs'
   use 'terrortylor/nvim-comment'
-
   use 'folke/which-key.nvim'
-  use 'github/copilot.vim'
-
-  use {
-    'VonHeikemen/lsp-zero.nvim',
-    requires = {
-      -- LSP Support
-      { 'neovim/nvim-lspconfig' },
-      { 'williamboman/mason.nvim' },
-      { 'williamboman/mason-lspconfig.nvim' },
-
-      -- Autocompletion
-      { 'hrsh7th/nvim-cmp' },
-      { 'hrsh7th/cmp-buffer' },
-      { 'hrsh7th/cmp-path' },
-      { 'saadparwaiz1/cmp_luasnip' },
-      { 'hrsh7th/cmp-nvim-lsp' },
-      { 'hrsh7th/cmp-nvim-lua' },
-
-      -- Snippets
-      { 'L3MON4D3/LuaSnip' },
-      { 'rafamadriz/friendly-snippets' },
-    }
-  }
 
   if packer_bootstrap then
     require('packer').sync()
