19cffa64a33023f25cfd1eef067af484bd483b1e
Avrel <106798140+av-rel@users.noreply.github.com>
2023-03-01 13:24:29 +0545

    dap and image

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,17 +1,27 @@
 return function(use)
 
-    use 'lewis6991/impatient.nvim'
+    use "lewis6991/impatient.nvim"
+    use "nvim-lua/plenary.nvim"
+
     use "catppuccin/nvim"
-    use 'nvim-tree/nvim-tree.lua'
-    use 'kyazdani42/nvim-web-devicons'
-    use 'christoomey/vim-tmux-navigator'
-    use 'ThePrimeagen/harpoon'
-    use 'windwp/nvim-autopairs'
-    use 'terrortylor/nvim-comment'
-    use 'romgrk/barbar.nvim'
-    use 'TimUntersberger/neogit'
-    use 'sindrets/diffview.nvim'
-    use 'github/copilot.vim'
+    use "kyazdani42/nvim-web-devicons"
+
+    use "nvim-tree/nvim-tree.lua"
+    use "christoomey/vim-tmux-navigator"
+    use "romgrk/barbar.nvim"
+    use "ThePrimeagen/harpoon"
+
+    use "windwp/nvim-autopairs"
+    use "terrortylor/nvim-comment"
+
+    use "TimUntersberger/neogit"
+    use "sindrets/diffview.nvim"
+
+    use "samodostal/image.nvim"
+    use "m00qek/baleia.nvim"
+    use "mfussenegger/nvim-dap"
+
+    use "github/copilot.vim"
 
 end
 
