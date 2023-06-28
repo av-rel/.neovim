a9aafae669b6272fd28cc9b0561a8322174c1fa8
Avrel <106798140+av-rel@users.noreply.github.com>
2023-06-28 08:18:20 +0545

    Installable plugs

--- init.lua
+++ init.lua
@@ -194,10 +194,7 @@ end
 --  Add any additional override configuration in the following tables. They will be passed to
 --  the `settings` field of the server config. You must look up that documentation yourself.
 local servers = {
-  clangd = {},
   pyright = {},
-  gopls = {},
-  rust_analyzer = {},
   emmetls = {},
   cssls = {},
   tsserver = {},
